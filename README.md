7729c6eef6bd1ad689caf0f83d0a3da26e1a3808
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-11-12 19:06:46 +0800

    finished SQL raw for statistics; fixed urls; check progress in README

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# database_proj
+# database_proj - zanette's part
 
-The database name is configured as `music_store` in the `/music_store/settings.py` file, so please set that as the name of your local database.
-
-for
 * statistics
+  * done with SQL raw
+  * needs to render to html
 * new music
 * arrival of music
 
->I have no idea how to test my code at the moment
+>I have no idea how to test my code at the moment because I need to load the DB which I haven't figure out yet either
--- statistics/views.py
+++ statistics/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.db import connection
 
 def index(request):
-    return HttpResponse("Find Statistics!")
+    return HttpResponse("Statistics function is working!")
 
 '''
 stats_choice reads the choice of statistics that user wants to be returned
@@ -14,39 +14,79 @@ depending on choice, load the different methods to query
 returned value of the functions below would be in INT
 '''
 def stats_choice(request,choice,top_no):
-    if choice is "song":
+    if choice == "song":
         return HttpResponse(song_stats(top_no))
-    elif choice is "artist":
+    elif choice == "artist":
         return HttpResponse(artist_stats(top_no))
-    elif choice is "genre":
+    elif choice == "genre":
         return HttpResponse(genre_stats(top_no))
-    elif choice is "all":
+    elif choice == "all":
         return song_stats(top_no),artist_stats(top_no),genre_stats(top_no),labels_stats(top_no)
     else:
-        return HttpResponse("Invalid Choice. Options: song,artist,genre,label and all.")
+        return HttpResponse("Invalid Choice. Options: song,artist,genre,label and all. Your choice:" + choice)
 #displays user stated number of top songs
-def song_stats(self,top_no):
+def song_stats(top_no):
     song_names = []
+    query_length = top_no
+    exceeded = False
     with connection.cursor() as cursor:
-        '''get no. of time songs got bought and the song id '''
-        cursor.execute("SELECT COUNT(*) FROM Users_Songs GROUP BY sid ORDER BY DESC")
+        # cursor.execute("SELECT COUNT(*) FROM Users_Songs GROUP BY sid ORDER BY DESC")
+        cursor.execute("SELECT S.name FROM Users_Songs US, Songs S WHERE US.sid = S.sid GROUP BY US.sid ORDER BY COUNT(*) DESC")
         results = cursor.fetchall() # get all the results
-        for i in range(0,top_no + 1):
-            sid = results[i][0]
-            cursor.execute("SELECT name FROM Songs WHERE sid = %s",[sid]) #i have no idea how to join atm until i try on mysql cli itself lol
-            name = cursor.fetchone()
+
+        #to handle if requested no. of top exceeds queried number
+        if (len(results) < top_no):
+            query_length = len(results)
+            exceeded = True
+
+        #results returned should be in ((xxx),(yyy))
+        for i in range(query_length):
+            name = results[i][0]
             song_names.append(name)
+
+    if exceeded :
+        song_names.append("Sorry! We could only return the top " + str(query_length))
     return song_names
 
 #displays user stated number of top artists
 #TODO: count no. of time artist appeared in the Users_Songs
-def artist_stats(self,top_no):
+def artist_stats(top_no):
     artist_names = []
+    query_length = top_no
+    exceeded = False
     with connection.cursor() as cursor:
-        '''get no. of time songs got bought and the song id '''
-        cursor.execute("SELECT COUNT(*) FROM Users_Songs JOIN Songs JOIN Artists GROUP BY sid ORDER BY DESC")
+        cursor.execute("SELECT A.name FROM Users_Songs US, Songs S, Artists A WHERE US.sid = S.sid AND S.aid = A.aid GROUP BY A.aid ORDER BY COUNT(*) DESC")
         results = cursor.fetchall() # get all the results
+
+        #to handle if requested no. of top exceeds queried number
+        if (len(results) < top_no):
+            query_length = len(results)
+            exceeded = True
+
+        for i in range(query_length):
+            name = results[i][0]
+            artist_names.append(name)
+    if exceeded :
+        artist_names.append("Sorry! We could only return the top " + str(query_length))
     return artist_names
-#displays user stated number of top  genre
-def genre_stats(self,top_no):
-    return
+
+#displays user stated number of top genre
+def genre_stats(top_no):
+    genre_names = []
+    query_length = top_no
+    exceeded = False
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT G.name FROM Users_Songs US, Songs S, Genres G WHERE US.sid = S.sid AND G.gid = S.gid GROUP BY G.aid ORDER BY COUNT(*) DESC")
+        results = cursor.fetchall() # get all the results
+
+        #to handle if requested no. of top exceeds queried number
+        if (len(results) < top_no):
+            query_length = len(results)
+            exceeded = True
+
+        for i in range(top_no):
+            name = results[i][0]
+            genre_names.append(name)
+    if exceeded :
+        genre_names.append("Sorry! We could only return the top " + str(query_length))
+    return genre_names
