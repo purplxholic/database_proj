9103605a6582879a1c18db152d9bdc4867af9477
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-11-12 21:13:41 +0800

    fixed schema for license tracking and typo for readme

--- schema.sql
+++ schema.sql
@@ -1,37 +1,39 @@
 CREATE TABLE Users (uid CHAR(10) PRIMARY KEY,
-					login VARCHAR(50) UNIQUE NOT NULL, 
+					login VARCHAR(50) UNIQUE NOT NULL,
 					password VARCHAR(20) NOT NULL);
-                                        
+
 CREATE TABLE Artists (aid CHAR(10) PRIMARY KEY,
-					name VARCHAR(50) NOT NULL);		                                        
-                                        
+					name VARCHAR(50) NOT NULL);
+
 CREATE TABLE Genres (gid CHAR(10) PRIMARY KEY,
 					name VARCHAR(50) NOT NULL);
-                                        
+
 CREATE TABLE Songs (sid CHAR(10) PRIMARY KEY,
 					name VARCHAR(50) NOT NULL,
 					aid CHAR(10) NOT NULL,
-					gid CHAR(10),                                                    
+					gid CHAR(10),
 					releaseDate DATE NOT NULL,
-					numDownloads INTEGER NOT NULL DEFAULT 0, 
+					numDownloads INTEGER NOT NULL DEFAULT 0,
+					numLicense INTEGER NOT NULL,
+					CHECK (numDownloads <= numLicense),
 					FOREIGN KEY (aid) REFERENCES Artists (aid),
-					FOREIGN KEY (gid) REFERENCES Genres (gid));																
-                                                    
+					FOREIGN KEY (gid) REFERENCES Genres (gid));
+
 CREATE TABLE Users_Songs (uid CHAR(10),
 						sid CHAR(10),
 	                    PRIMARY KEY (uid, sid),
 	                    FOREIGN KEY (uid) REFERENCES Users (uid),
 	                    FOREIGN KEY (sid) REFERENCES Songs (sid));
-                                                    
+
 CREATE TABLE Feedbacks (uid CHAR(10) NOT NULL,
-						sid CHAR(10) NOT NULL, 
+						sid CHAR(10) NOT NULL,
                         score INTEGER NOT NULL CHECK (score>=0 AND score<=10),
                         postDate DATE NOT NULL,
                         comments VARCHAR(250),
                         PRIMARY KEY (uid,sid),
                         FOREIGN KEY (uid) REFERENCES Users (uid),
                         FOREIGN KEY (sid) REFERENCES Songs (sid));
-                                                
+
 CREATE TABLE Ratings (uid CHAR(10),
 					fuid CHAR(10),
 					sid CHAR(10),
@@ -39,4 +41,3 @@ CREATE TABLE Ratings (uid CHAR(10),
 					PRIMARY KEY(uid, fuid, sid),
 					FOREIGN KEY (fuid, sid) REFERENCES Feedbacks (uid,sid),
                     CHECK (uid <> fuid));
-                                        
