38d921b2f4a61aeb940efa3d66ad623aba698b5d
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-11-20 17:49:39 +0800

    fixed code for statistics

--- statistics/views.py
+++ statistics/views.py
@@ -27,66 +27,66 @@ def stats_choice(request,choice,top_no):
 #displays user stated number of top songs
 def song_stats(top_no):
     song_names = []
-    query_length = top_no
+    query_length = int(top_no)
     exceeded = False
     with connection.cursor() as cursor:
-        # cursor.execute("SELECT COUNT(*) FROM Users_Songs GROUP BY sid ORDER BY DESC")
-        cursor.execute("SELECT S.name FROM Users_Songs US, Songs S WHERE US.sid = S.sid GROUP BY US.sid ORDER BY COUNT(*) DESC")
+        # cursor.execute("SELECT COUNT(*) FROM Purchases GROUP BY sid ORDER BY DESC")
+        cursor.execute("SELECT S.name FROM Purchases P, Songs S WHERE P.sid = S.sid GROUP BY P.sid ORDER BY COUNT(*) DESC")
         results = cursor.fetchall() # get all the results
 
         #to handle if requested no. of top exceeds queried number
-        if (len(results) < top_no):
+        if (len(results) < query_length):
             query_length = len(results)
             exceeded = True
 
         #results returned should be in ((xxx),(yyy))
         for i in range(query_length):
             name = results[i][0]
-            song_names.append(name)
+            song_names.append(name + " ")
 
     if exceeded :
-        song_names.append("Sorry! We could only return the top " + str(query_length))
+        song_names.append("\nSorry! We could only return the top " + str(query_length))
     return song_names
 
 #displays user stated number of top artists
-#TODO: count no. of time artist appeared in the Users_Songs
+#TODO: count no. of time artist appeared in the Purchases
 def artist_stats(top_no):
     artist_names = []
-    query_length = top_no
+    query_length = int(top_no)
     exceeded = False
     with connection.cursor() as cursor:
-        cursor.execute("SELECT A.name FROM Users_Songs US, Songs S, Artists A WHERE US.sid = S.sid AND S.aid = A.aid GROUP BY A.aid ORDER BY COUNT(*) DESC")
+        cursor.execute("SELECT A.name FROM Purchases P, Songs S, Artists A WHERE P.sid = S.sid AND S.aid = A.aid GROUP BY A.aid ORDER BY COUNT(*) DESC")
         results = cursor.fetchall() # get all the results
 
         #to handle if requested no. of top exceeds queried number
-        if (len(results) < top_no):
+        if (len(results) < query_length):
             query_length = len(results)
             exceeded = True
 
         for i in range(query_length):
             name = results[i][0]
-            artist_names.append(name)
+            artist_names.append(name + " ")
     if exceeded :
-        artist_names.append("Sorry! We could only return the top " + str(query_length))
+        artist_names.append("\nSorry! We could only return the top " + str(query_length))
     return artist_names
 
 #displays user stated number of top genre
 def genre_stats(top_no):
     genre_names = []
-    query_length = top_no
+    query_length = int(top_no)
     exceeded = False
     with connection.cursor() as cursor:
-        cursor.execute("SELECT G.name FROM Users_Songs US, Songs S, Genres G WHERE US.sid = S.sid AND G.gid = S.gid GROUP BY G.aid ORDER BY COUNT(*) DESC")
+        cursor.execute("SELECT G.name FROM Purchases P, Songs S, Genres G WHERE P.sid = S.sid AND G.gid = S.gid GROUP BY G.aid ORDER BY COUNT(*) DESC")
         results = cursor.fetchall() # get all the results
 
         #to handle if requested no. of top exceeds queried number
-        if (len(results) < top_no):
-            query_length = len(results)
+        if (len(results) < query_length):
+            query_length = len(results + " ")
             exceeded = True
 
         for i in range(top_no):
             name = results[i][0]
             genre_names.append(name)
     if exceeded :
-        genre_names.append("Sorry! We could only return the top " + str(query_length))
+        genre_names.append("\nSorry! We could only return the top " + str(query_length))
     return genre_names
